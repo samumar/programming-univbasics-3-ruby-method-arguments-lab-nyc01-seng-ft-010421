c29ef540b1619f33e6c6644bbd33883093146fd1
samumar43 <samumar43@gmail.com>
2020-09-27 15:15:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,9 +11,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-def introduction_with_language_optional(name, language = "Ruby") do
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
-end 
+
 introduction_with_language_optional("Edwin")
 end
 
